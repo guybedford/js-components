86165f0ec950879c81ec74145472b003789bf08c
Guy Bedford <guybedford@gmail.com>
2022-06-26 21:48:15 -0700

    crate structuring

---
+++ wasm/src/lib.rs
@@ -0,0 +1,10 @@
+use wasm_bindgen::prelude::*;
+use js_components;
+
+#[wasm_bindgen]
+pub fn parse(bytes: &[u8]) -> Result<String, String> {
+	match js_components::parse(bytes) {
+		Ok(serialized) => Ok(serialized),
+		Err(err) => Err(String::from(format!("{:?}", err))),
+	}
+}
